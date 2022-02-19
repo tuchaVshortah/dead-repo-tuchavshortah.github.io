1e26809cb2a75495522b91ed136f1ff05b646b21
tuchaVshortah <71591558+tuchaVshortah@users.noreply.github.com>
2022-02-19 11:53:17 +0600

    Add files via upload

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -26,9 +26,9 @@ function readCookie(name) {
 }
 
 window.onload = function(){
-    var delay = 3000;
+    var delay = 5000;
     var url = 'http://tuchavshortah.github.io/pages/main.html';
-    setTimeout(function () {
+    setTimeout(function(){
         $(document).prop('title', 'Redirecting...');
         window.location.replace(url);
         }, delay
